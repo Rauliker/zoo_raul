9d928b36f200d5815e6eb72ab960a8aa0580e025
Raul <rauliker04@gmail.com.com>
2025-02-06 18:36:21 +0100

    agrega campos de estado y fecha de muerte en el modelo de animales, y actualiza vistas para reflejar estos cambios

--- models/animal.py
+++ models/animal.py
@@ -2,7 +2,6 @@ import uuid
 from datetime import date
 from odoo.exceptions import ValidationError
 from odoo import fields, models, api
-
 class ZooAnimal(models.Model):
     _name = "zoo.animal"
 
@@ -58,11 +57,32 @@ class ZooAnimal(models.Model):
         domain=[('id', '!=', False)],
         ondelete="cascade",  
     )
+    status = fields.Selection(
+        string="Status",
+        selection=[
+            ('alive', 'Alive'),
+            ('sick', 'Sick'),
+            ('dead', 'Dead'),
+        ],
+        default='alive',
+        required=True
+    )
+    date_of_death = fields.Date(
+        string="Date of Death",
+        store=True
+    )
 
-    @api.depends('date_birth')
+    @api.depends('date_birth', 'status', 'date_of_death')
     def _compute_age(self):
         for record in self:
-            if record.date_birth:
+            if record.status == 'dead' and record.date_of_death:
+                death_date = fields.Date.from_string(record.date_of_death)
+                birth_date = fields.Date.from_string(record.date_birth)
+                record.age = (
+                    death_date.year - birth_date.year
+                    - ((death_date.month, death_date.day) < (birth_date.month, birth_date.day))
+                )
+            elif record.status == 'alive' and record.date_birth:
                 today = date.today()
                 birth_date = fields.Date.from_string(record.date_birth)
                 record.age = (
@@ -79,3 +99,8 @@ class ZooAnimal(models.Model):
                 raise ValidationError(
                     "The date of birth must be earlier than today."
                 )
+
+    @api.onchange('status')
+    def _onchange_status(self):
+        if self.status == 'dead' and not self.date_of_death:
+            self.date_of_death = fields.Date.today()

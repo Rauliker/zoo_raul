2b1da225fcc7592d52043f11948e2b71ed2928ae
Raul <rauliker04@gmail.com.com>
2025-02-10 16:41:00 +0100

    agrega decoraciones visuales para el árbol de animales según el tipo de alimento y añade archivos de estilos CSS

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Zoologia Raul',
     'application': True,
-    'depends': ['base','web',],
+    'depends': ['base', 'web'],
     'data': [
         'security/ir.model.access.csv',
         'views/zoo_views.xml',
@@ -9,8 +9,15 @@
         'views/animal_views.xml',
         'views/city_views.xml',
         'views/continent_views.xml', 
-        'views/estate_menus.xml', 
-
+        'views/estate_menus.xml',
     ],
+    'assets': {
+        'web.assets_frontend': [
+            'zoo_raul/static/src/css/styles.css',
+        ],
+        'web.assets_backend': [
+            'zoo_raul/static/src/scss/styles.scss',
+        ],
+    },
     'icon': '/zoo_raul/static/description/icon.png',
 }
